93ad90db6a6655c5cc950b2d2e0d4b0d44345d1e
Sachin-Tharaka <tharakasachin98@gmail.com>
2024-08-13 10:43:22 +0530

    add filters to agents, users

--- src/components/ActiveUsers.js
+++ src/components/ActiveUsers.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { fetchActiveUsers } from "../services/UserService";
 import "../styles/ActiveUsers.css";
 
 const ActiveUsers = () => {
   const [activeUsers, setActiveUsers] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [startTime, setStartTime] = useState(new Date(0).toISOString());
-  const [endTime, setEndTime] = useState(new Date().toISOString());
+  const [startTime, setStartTime] = useState(new Date(0));
+  const [endTime, setEndTime] = useState(new Date());
 
   useEffect(() => {
-    fetchActiveUsers(startTime, endTime)
+    // Check if startTime is before endTime
+    if (startTime > endTime) {
+      console.warn("Start date should be before end date.");
+      return;
+    }
+
+    const formattedStartTime = startTime.toISOString();
+    const formattedEndTime = endTime.toISOString();
+
+    fetchActiveUsers(formattedStartTime, formattedEndTime)
       .then((data) => {
         setActiveUsers(data);
-        console.log(startTime, endTime);
         console.log("Active users data:", data);
         setLoading(false);
       })
@@ -23,18 +33,32 @@ const ActiveUsers = () => {
       });
   }, [startTime, endTime]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="detail-container">
       <h1 className="detail-header">Active User Details</h1>
-      <div className="detail-content">
-        {activeUsers.length === 0 ? (
-          <p>No active users at the moment.</p>
-        ) : (
-          activeUsers.map((user) => (
+
+      <div className="date-picker-container">
+        <DatePicker
+          selected={startTime}
+          onChange={(date) => setStartTime(date)}
+          dateFormat="yyyy/MM/dd"
+          className="date-picker"
+        />
+        <DatePicker
+          selected={endTime}
+          onChange={(date) => setEndTime(date)}
+          dateFormat="yyyy/MM/dd"
+          className="date-picker"
+        />
+      </div>
+
+      {loading ? (
+        <div className="loading-text">Loading...</div>
+      ) : activeUsers.length === 0 ? (
+        <div className="no-data-text">No active users at the moment.</div>
+      ) : (
+        <div className="detail-content">
+          {activeUsers.map((user) => (
             <div key={user.userId} className="user-details">
               <h2>User ID: {user.userId}</h2>
               <p>
@@ -55,9 +79,10 @@ const ActiveUsers = () => {
                 {new Date(user.createdAt).toLocaleDateString()}
               </p>
             </div>
-          ))
-        )}
-      </div>
+          ))}
+        </div>
+      )}
+
       <Link to="/overview" className="back-link">
         Back to Overview
       </Link>
--- src/components/AgentDetails.js
+++ src/components/AgentDetails.js
@@ -8,14 +8,29 @@ import { fetchActiveKennels } from "../services/KennelService";
 const AgentDetails = () => {
   const [agentDetails, setAgentDetails] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [startTime, setStartTime] = useState(new Date(0).toISOString());
-  const [endTime, setEndTime] = useState(new Date().toISOString());
+  const [startTime, setStartTime] = useState(new Date(0));
+  const [endTime, setEndTime] = useState(new Date());
 
   useEffect(() => {
-    fetchActiveKennels(startTime, endTime)
+    // Check if startTime is before endTime
+    if (startTime > endTime) {
+      console.warn("Start date should be before end date.");
+      return;
+    }
+
+    const formattedStartTime = startTime.toISOString();
+    const formattedEndTime = endTime.toISOString();
+
+    fetchActiveKennels(formattedStartTime, formattedEndTime)
       .then((data) => {
-        setAgentDetails(data);
+        // Filter data based on the date range
+        const filteredData = data.filter((agent) => {
+          const createdDate = new Date(agent.createdDate);
+          return createdDate >= startTime && createdDate <= endTime;
+        });
+        setAgentDetails(filteredData);
         setLoading(false);
+        console.log("filtered data", filteredData);
       })
       .catch((error) => {
         console.error("Error fetching agent details:", error);
@@ -23,58 +38,73 @@ const AgentDetails = () => {
       });
   }, [startTime, endTime]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (agentDetails.length === 0) {
-    return <div>No agent details available.</div>;
-  }
-
   return (
     <div className="detail-container">
-      <h1 className="detail-header">Agent Details</h1>
-      <div className="detail-content">
-        {agentDetails.map((agent) => (
-          <div key={agent.kennelId} className="agent-detail-item">
-            <h2>{agent.kennelName}</h2>
-            <p>
-              <strong>Kennel ID:</strong> {agent.kennelId}
-            </p>
-            <p>
-              <strong>Created Date:</strong>{" "}
-              {new Date(agent.createdDate).toLocaleString()}
-            </p>
+      <h1 className="detail-header">Active Agent Details</h1>
 
-            <div className="agent-address">
+      <div className="date-picker-container">
+        <DatePicker
+          selected={startTime}
+          onChange={(date) => setStartTime(date)}
+          dateFormat="yyyy/MM/dd"
+          className="date-picker"
+        />
+        <DatePicker
+          selected={endTime}
+          onChange={(date) => setEndTime(date)}
+          dateFormat="yyyy/MM/dd"
+          className="date-picker"
+        />
+      </div>
+
+      {loading ? (
+        <div>Loading...</div>
+      ) : agentDetails.length === 0 ? (
+        <div>No agent details available.</div>
+      ) : (
+        <div className="detail-content">
+          {agentDetails.map((agent) => (
+            <div key={agent.kennelId} className="agent-detail-item">
+              <h2>{agent.kennelName}</h2>
               <p>
-                <strong>Address:</strong>
+                <strong>Kennel ID:</strong> {agent.kennelId}
               </p>
               <p>
-                {agent.kennelAddress.address1} {agent.kennelAddress.address2}
+                <strong>Created Date:</strong>{" "}
+                {new Date(agent.createdDate).toLocaleDateString()}
+              </p>
+
+              <div className="agent-address">
+                <p>
+                  <strong>Address:</strong>
+                </p>
+                <p>
+                  {agent.kennelAddress.address1} {agent.kennelAddress.address2}
+                </p>
+                <p>
+                  {agent.kennelAddress.city}, {agent.kennelAddress.zipCode}
+                </p>
+              </div>
+
+              <p>
+                <strong>Location:</strong> Latitude:{" "}
+                {agent.kennelLocation.coordinates[0]}, Longitude:{" "}
+                {agent.kennelLocation.coordinates[1]}
               </p>
               <p>
-                {agent.kennelAddress.city}, {agent.kennelAddress.zipCode}
+                <strong>Owner Name:</strong> {agent.ownerName}
+              </p>
+              <p>
+                <strong>Owner Email:</strong> {agent.ownerEmail}
+              </p>
+              <p>
+                <strong>Owner Phone:</strong> {agent.ownerPhone || "N/A"}
               </p>
             </div>
+          ))}
+        </div>
+      )}
 
-            <p>
-              <strong>Location:</strong> Latitude:{" "}
-              {agent.kennelLocation.coordinates[0]}, Longitude:{" "}
-              {agent.kennelLocation.coordinates[1]}
-            </p>
-            <p>
-              <strong>Owner Name:</strong> {agent.ownerName}
-            </p>
-            <p>
-              <strong>Owner Email:</strong> {agent.ownerEmail}
-            </p>
-            <p>
-              <strong>Owner Phone:</strong> {agent.ownerPhone || "N/A"}
-            </p>
-          </div>
-        ))}
-      </div>
       <Link to="/overview" className="back-link">
         Back to Overview
       </Link>
